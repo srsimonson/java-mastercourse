e4f53de3ff0d989f0d30b2953142460f62418877
Steve Simonson <srsimonson@gmail.com>
2020-09-24 00:01:06 -0500

    basic terms complete

---
+++ src/OperatorChallenge.java
@@ -0,0 +1,16 @@
+public class OperatorChallenge {
+    public static void main(String[] args) {
+
+        double var1 = 20.00d;
+        double var2 = 80.00d;
+        double var3 = (var1 + var2) * 100.00d;
+        double var4 = var3 % 40.00d;
+        System.out.println("var4: " + var4);
+        boolean isNoRemainder = (var4 == 0) ? true : false;
+        System.out.println(isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder.");
+        }
+    
+    }
+}
--- src/OperatorsOperandsExpressions.java
+++ src/OperatorsOperandsExpressions.java
@@ -23,5 +23,41 @@ public class OperatorsOperandsExpressions {
         System.out.println(result *= 10);
         System.out.println(result ++); // this doesn't work for some reason
         System.out.println(result /= 2);
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+        System.out.println("___________");
+
+        int firstScore = 81;
+        int secondScore = 60;
+        if (firstScore > secondScore && firstScore < 100) {
+            System.out.println("You got the top score!");
+        }
+        // & and | are also logical operators in Java. They are bitwise, wirking at the bit level... TBD!
+
+        if ( (firstScore > 90) || (secondScore <= 90)) {
+            System.out.println("Either or both conditions are true!");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("This is not an error");
+        }
+
+        // Ternary Operators
+        boolean isCar = false;
+        if (isCar) {
+            System.out.println("This is not supposed to happen.");
+        }
+
+        isCar = true;
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("wasCar is true");
+        }
+
     }
 }
