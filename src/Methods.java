a9916a7402060ca96f344d2ce58a05488da126c2
Steve Simonson <srsimonson@gmail.com>
2020-10-03 20:59:41 -0500

    method with parameters

--- src/Methods.java
+++ src/Methods.java
@@ -1,6 +1,14 @@
 public class Methods {
- 
-    public static void main (String[] args) {
-        System.out.println("hi");
+
+    public static void main(String[] args) {
+        calculateScore(true, 800, 5, 100);
+        calculateScore(true, 10000, 8, 200);
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver == true) { // also if (gameOver) works
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was: " + finalScore);
+        }
     }
 }
