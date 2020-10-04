20d1e39a9c57d76b96fce33c9a419716a506d338
Steve Simonson <srsimonson@gmail.com>
2020-10-03 21:33:10 -0500

    methods

--- src/Methods.java
+++ src/Methods.java
@@ -2,13 +2,20 @@ public class Methods {
 
     public static void main(String[] args) {
         calculateScore(true, 800, 5, 100);
-        calculateScore(true, 10000, 8, 200);
+        calculateScore(false, 10000, 8, 200);
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver == true) { // also if (gameOver) works
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was: " + finalScore);
-        }
+            return finalScore;
+            // Could add an if/else if you wanted to. Otherwise this way works too.
+        } 
+        System.out.println("-1");
+        return -1;
+        // Why return -1? Conventionally used to indicate an error. Searching algorithms imply -1 means "value not found."
     }
 }
+
+// VOID means "don't return anything." Change VOID to INT to return a value to the top method. In Java, you have to return something. It will error if nothing is returned, as in the 1st if statement isn't met. Need the other return.
