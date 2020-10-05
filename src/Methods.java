7ba1427ac94f0a39d6732a41b19e269f89734768
Steve Simonson <srsimonson@gmail.com>
2020-10-04 22:49:35 -0500

    challenge next

--- src/Methods.java
+++ src/Methods.java
@@ -1,14 +1,23 @@
 public class Methods {
 
     public static void main(String[] args) {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
         calculateScore(true, 800, 5, 100);
         calculateScore(false, 10000, 8, 200);
+
+        int highScore = calculateScore(gameOver, score, levelCompleted, bonus);
+        System.out.println(highScore);
     }
 
     public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver == true) { // also if (gameOver) works
             int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was: " + finalScore);
+
             return finalScore;
             // Could add an if/else if you wanted to. Otherwise this way works too.
         } 
